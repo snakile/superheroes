61782ec7dc05ecbbaec5ea7ece246dc0f7e5f3a2
Yonatan <snakile@gmail.com>
2018-02-01 23:14:51 +0200

    Extract a fetcher class

--- fetcher.py
+++ fetcher.py
@@ -1,19 +1,22 @@
 import requests
 
 
-def fetch(params):
-    url = 'https://comicvine.gamespot.com/api/characters'
-    base_params = {'api_key': '969a888c5b1043890fb27bbf267ace9d4d7e0fd8', 'format': 'json'}
-    params = {**base_params, **params}
-    response = requests.get(url=url, params=params, headers={'User-agent': 'foo'})
-    data = response.json()
-    return data
+class Fetcher:
+    def __init__(self):
+        self.url = 'https://comicvine.gamespot.com/api/characters'
+        self.base_params = {'api_key': '969a888c5b1043890fb27bbf267ace9d4d7e0fd8', 'format': 'json'}
+
+    def fetch(self, params):
+        params = {**self.base_params, **params}
+        response = requests.get(url=self.url, params=params, headers={'User-agent': 'foo'})
+        data = response.json()
+        return data
 
 
 def main():
     params = {'field_list': 'gender,name,powers,origin,id',
               'limit': '2'}
-    data = fetch(params)
+    data = Fetcher().fetch(params)
     print(data['results'][0]['name'])
 
 
