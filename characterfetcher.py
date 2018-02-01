5d92c50b5c028267cebc84510081c21150927b26
Yonatan <snakile@gmail.com>
2018-02-02 00:56:28 +0200

    Begin the implementation of the single character fetcher

--- characterfetcher.py
+++ characterfetcher.py
@@ -6,13 +6,11 @@ class CharacterFetcher:
     def __init__(self):
         self.base_url = 'https://comicvine.gamespot.com/api/'
         self.base_params = {'api_key': '969a888c5b1043890fb27bbf267ace9d4d7e0fd8', 'format': 'json'}
-        self.character_field_list = ','.join(['name', 'gender', 'origin', 'powers'])
 
     def _fetch(self, api, params):
         url = urllib.parse.urljoin(self.base_url, api)
         params = {**self.base_params, **params}
         response = requests.get(url=url, params=params, headers={'User-agent': 'foo'})
-        print(response.url)
         results = response.json()['results']
         return results
 
@@ -21,10 +19,11 @@ class CharacterFetcher:
         params = {**params, **default_params}
         return self._fetch('characters', params)
 
-    def _fetch_single_character_api(self, params):
-        default_params = {'field_list': 'id'}
+    def _fetch_single_character_api(self, params, character_id):
+        character_field_list = ['name', 'gender', 'origin', 'powers']
+        default_params = {'field_list': ','.join(character_field_list)}
         params = {**params, **default_params}
-        return self._fetch('character', params)
+        return self._fetch('character/{}'.format(character_id), params)
 
     def get_male_characters(self, n=100):
         params = {'filter': 'gender:1', 'limit': str(n)}
@@ -33,13 +32,19 @@ class CharacterFetcher:
 
     def get_character(self, character_id):
         id_prefix = '4005'
-        # character_id = '-'.join(id_prefix, character_id)
+        character_id = '-'.join([id_prefix, character_id])
+        return self._fetch_single_character_api({}, character_id)
 
 
 def main():
     fetcher = CharacterFetcher()
     male_characters = fetcher.get_male_characters(10)
     print(list(male_characters))
+    character_json = fetcher.get_character('1264')
+    print(character_json['name'])
+    print(character_json['gender'])
+    print(character_json['origin']['name'])
+    print(character_json['powers'])
 
 
 if __name__ == '__main__':
