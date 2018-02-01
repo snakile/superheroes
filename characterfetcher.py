9bd23eee12891235f4accb6044c53c3bce550ba7
Yonatan <snakile@gmail.com>
2018-02-01 23:31:50 +0200

    Implement a male character fetcher

--- characterfetcher.py
+++ characterfetcher.py
@@ -4,20 +4,24 @@ import requests
 class CharacterFetcher:
     def __init__(self):
         self.url = 'https://comicvine.gamespot.com/api/characters'
-        self.base_params = {'api_key': '969a888c5b1043890fb27bbf267ace9d4d7e0fd8', 'format': 'json'}
+        self.base_params = {'api_key': '969a888c5b1043890fb27bbf267ace9d4d7e0fd8', 'format': 'json', 'field_list': 'id'}
 
     def _fetch(self, params):
         params = {**self.base_params, **params}
         response = requests.get(url=self.url, params=params, headers={'User-agent': 'foo'})
-        data = response.json()
-        return data
+        results = response.json()['results']
+        return results
+
+    def fetch_male_characters(self, n=100):
+        params = {'filter': 'gender:1', 'limit': str(n)}
+        results = self._fetch(params)
+        return (result['id'] for result in results)
 
 
 def main():
-    params = {'field_list': 'gender,name,powers,origin,id',
-              'limit': '2'}
-    data = CharacterFetcher()._fetch(params)
-    print(data['results'][0]['name'])
+    fetcher = CharacterFetcher()
+    male_characters = fetcher.fetch_male_characters(10)
+    print(list(male_characters))
 
 
 if __name__ == '__main__':
