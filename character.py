f20e1beda2ee0f328d51775b54c5f692602a0c98
Yonatan <snakile@gmail.com>
2018-02-02 01:04:43 +0200

    Implement a Character class

---
+++ character.py
@@ -0,0 +1,12 @@
+class Character:
+    def __init__(self, name, gender, origin, powers):
+        self.name = name
+        self.gender = gender
+        self.origin = origin
+        self.powers = powers
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return 'name: {}, gender: {}, origin: {} \n powers: {}'.format(self.name, self.gender, self.origin, self.powers)
